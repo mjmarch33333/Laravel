bef7332a212b4b7258a66ef445b9a4188bec3cee
mjmarch33333 <mitch3_333@yahoo.com>
2015-08-03 15:47:17 -0400

    test2
    test2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\Inspire::class,
+        \App\Console\Commands\NotifyOneDaySMS::class,
+        \App\Console\Commands\NotifyOneHourSMS::class,
     ];
 
     /**
