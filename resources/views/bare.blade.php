618cdcaa0c59e8d28e373d667626a6c8081c5633
marchmikej <marchmikej@gmail.com>
2015-09-30 04:07:06 -0400

    more testing
    more

--- resources/views/bare.blade.php
+++ resources/views/bare.blade.php
@@ -2,10 +2,14 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="http://rootedindezign.com/css/test.css?v=1">
+		<!-- http://davidbcalhoun.com/2010/viewport-metatag -->
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+
+	<!-- http://www.kylejlarson.com/blog/2012/iphone-5-web-design/ and http://darkforge.blogspot.fr/2010/05/customize-android-browser-scaling-with.html -->
+	<meta name="viewport" content="user-scalable=0, initial-scale=1.0, target-densitydpi=115">
 	<title>TenantSync</title>
-      
+      <link rel="stylesheet" href="http://rootedindezign.com/css/test.css?v=1">
 </head>
 
 	<body>
