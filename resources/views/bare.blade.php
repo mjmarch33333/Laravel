f559b076415a3f678394d57b9913e055f0616f74
marchmikej <marchmikej@gmail.com>
2015-09-30 04:02:56 -0400

    test 4
    test

--- resources/views/bare.blade.php
+++ resources/views/bare.blade.php
@@ -1,10 +1,11 @@
 <html class="no-js" lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://rootedindezign.com/css/test.css?v=1">
 	<title>TenantSync</title>
-        <link href="http://rootedindezign.com/css/test.css?v=1" rel="stylesheet" >
+      
 </head>
 
 	<body>
